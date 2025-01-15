24b21c009b6d4b99f16bfdd0e214b2109c0efb78
zhao.song <song0586@126.com>
2025-01-15 10:39:39 +0800

    feat: antlr4入门案例

---
+++ src/main/java/com/common/antlr4/MathMain.java
@@ -0,0 +1,26 @@
+package com.common.antlr4;
+
+//import com.common.antlr4.generated.MathLexer;
+//import com.common.antlr4.generated.MathParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+/**
+ * description: 测试Math.g4生成的语法
+ * company: 北京海量数据有限公司
+ * create by: zhaosong 2025/1/15
+ * version: 1.0
+ */
+public class MathMain {
+
+    public static void main(String[] args) {
+//        CodePointCharStream input = CharStreams.fromString("12*2+12\r\n");
+//        MathLexer lexer = new MathLexer(input);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        MathParser parser = new MathParser(tokens);
+//        ParseTree tree = parser.prog();// 解析
+//        System.out.println(tree);
+    }
+}
